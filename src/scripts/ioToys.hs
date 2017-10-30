9a51c6bcc6d9b2b81240b595b89b55a47a0ffcac
Felipe Masseo <felipe.masseo@teknisa.com>
2017-10-30 12:51:14 +0000

    New IO function added

--- src/scripts/ioToys.hs
+++ src/scripts/ioToys.hs
@@ -11,10 +11,25 @@ main = do
     if program == "describingThings"
     then do
         forM ["pretty", "boring", "average"] (\adj -> do  
-            putStrLn $ "Give me an it: "  
+            putStrLn "Give me an it: "  
             subj <- getLine
             putStrLn $ subj ++ " is " ++ adj
             return ())
+        return ()
+    else do
+    if program == "toisen"
+    then do
+        coefs <- forM ["a", "b", "c"] (\coefficient -> do
+            putStrLn $ "Choose a value for the coefficient " ++ coefficient
+            value <- getLine
+            return value)
+        let a = read $ coefs !! 0 :: Float
+            b = read $ coefs !! 1 :: Float
+            c = read $ coefs !! 2 :: Float
+            delta = (b * b) - 4 * a * c
+            root = (-b + sqrt delta) / (2 * a)
+            root' = (-b - sqrt delta) / (2 * a)
+        putStrLn $ "The first root is " ++ show root ++ " and the second one is " ++ show root'
         return ()
     else do
         putStrLn "We don't have this program, yet! How about implementing it?!"
